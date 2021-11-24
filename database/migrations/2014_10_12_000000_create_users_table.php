e55c197851e4c3fd29bfb910ddf642bfb8107fd0
Don_Silva <83092440+Silvadon101@users.noreply.github.com>
2021-11-24 14:31:11 -0500

    Modify Users Table and Created Roles Table etc
    Roles Table and its resources were also create

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,19 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->integer('role_id');
+            $table->integer('branch_id');
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('username')->unique();
+            $table->string('address')->nullable();
+            $table->string('city')->nullable();
+            $table->string('state')->nullable();
+            $table->string('pin')->nullable();
+            $table->string('country')->nullable();
+            $table->text('description')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('image')->default('default.png');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
